a26f4097daf98019d2a08dc673c4d7d34046fffd
Gaogao <392521254@qq.com>
2022-05-01 00:05:53 +0800

    移动端项目创建及页面编写

---
+++ codes/uniapp/uniapp/pages/index/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<view>
+		<u-navbar :is-back="false" title="首页" :background="background"></u-navbar>
+		<u-swiper :list="lunbolist"></u-swiper>
+
+		<u-grid :col="2">
+			<u-grid-item>
+				<image src="../../static/icons/hospital.png" class="img-80" ></image>
+				<view class="grid-text" style="font-size: 35upx;">医院介绍</view>
+			</u-grid-item>
+			<u-grid-item @tap="toYuyue()">
+				<image src="../../static/icons/doctor.png" class="img-80"></image>
+				<view class="grid-text" style="font-size: 35upx;">预约挂号</view>
+			</u-grid-item>
+			<u-grid-item >
+				<image  src="../../static/icons/daozhen.png" class="img-80"></image>
+				<view class="grid-text" style="font-size: 35upx;">自助导诊</view>
+			</u-grid-item>
+			<u-grid-item>
+				<image src="../../static/icons/history.png" class="img-80"></image>
+				<view class="grid-text" style="font-size: 35upx;">门诊记录</view>
+			</u-grid-item>
+		</u-grid>
+	</view>
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				background: {
+					backgroundColor: '#eeeeee',
+
+				},
+				lunbolist: [
+					'../../static/imgs/lunbotu1.png',
+					'../../static/imgs/lunbotu2.png',
+					'../../static/imgs/lunbotu3.png',
+				],
+			}
+		},
+		onLoad() {
+			
+		},
+		methods: {
+		toYuyue(){
+			uni.navigateTo({
+				url:"../yuyuegua/yuyuegua"
+			})
+		}
+		}
+	}
+</script>
+<style scoped lang="scss">
+	.grid-text {
+		font-size: 28rpx;
+		margin-top: 4rpx;
+		color: $u-type-info;
+	}
+</style>
---
+++ codes/uniapp/uniapp/pages/mine/mine.vue
@@ -0,0 +1,77 @@
+<template>
+	<view>
+		<u-navbar :is-back="false" title="我" :background="background"></u-navbar>
+		<view style="display: flex;margin: 30upx 25upx;">
+			<view style="flex-direction: row; ">
+				<u-image width="180upx" height="180rpx" shape="circle" style="margin-left: 20upx;" :src="imgsrc" />
+			</view>
+			<view style="margin: 20upx 40upx;">
+				<view style="font-size:40upx;color: #333333;">
+					李四
+				</view>
+				<view style="margin-top: 50upx;font-size: 30upx;color: #999999;">
+					用户名:
+				</view>
+			</view>
+
+		</view>
+		<u-cell-group style="margin-top:50upx;">
+			<u-cell-item icon="setting-fill" title="修改名称"></u-cell-item>
+			<u-cell-item icon="lock-fill" title="修改密码"></u-cell-item>
+		</u-cell-group>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				background: {
+					backgroundColor: '#eeeeee',
+
+				},
+				imgsrc: ''
+			}
+		},
+		
+		methods: {
+			load() {
+				//获取用户名、头像等信息
+				//如果判断为男生则img设为男生的头像
+				if (true) {
+					this.imgsrc = '../../static/icons/man.png'
+				} else {
+					this.imgsrc = '../../static/icons/female.png'
+				}
+
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	// .wrap {
+	// 	padding: 24rpx;
+	// }
+
+	// .u-row {
+	// 	margin: 40rpx 0;
+	// }
+
+	// .demo-layout {
+	// 	height: 80rpx;
+	// 	border-radius: 8rpx;
+	// }
+
+	// .bg-purple {
+	// 	background: #d3dce6;
+	// }
+
+	// .bg-purple-light {
+	// 	background: #e5e9f2;
+	// }
+
+	// .bg-purple-dark {
+	// 	background: #99a9bf;
+	// }
+</style>
